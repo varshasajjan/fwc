4b34c78d7375bb77c6ffcb9d0b2c2fcebaf30da6
varshasajjan <varshinigonchigar@gmail.com>
2025-09-04 22:06:21 +0530

    Update blink_2leds.asm

--- assembly/blink_2leds.asm
+++ assembly/blink_2leds.asm
@@ -1,66 +1,98 @@
-; Boolean Identity Checker
-; (x + y) ⊕ z  vs  x ⊕ (y + z)
-; ATmega328P, Arduino Uno
-; Inputs: PD2=x, PD3=y, PD4=z
-; Outputs: PB0 = LHS, PB1 = RHS
+; AVR Assembly for ATmega328P
+; Inputs: PD2 (x), PD3 (y), PD4 (z)
+; Outputs: PD6 (A), PD7 (B)
 
 .include "m328Pdef.inc"
 
-.def temp = r16
-.def xreg = r17
-.def yreg = r18
-.def zreg = r19
-.def lhs  = r20
-.def rhs  = r21
+; Define bit masks
+.equ X_BIT = (1<<2)
+.equ Y_BIT = (1<<3)
+.equ Z_BIT = (1<<4)
+.equ LED_A = (1<<6)
+.equ LED_B = (1<<7)
 
-.org 0x0000
-    rjmp RESET
+.cseg
+.org 0x00
 
-RESET:
-    ; ---- Setup I/O ----
-    ; PORTD2,3,4 as inputs
-    ldi temp, 0x00
-    out DDRD, temp
+rjmp MAIN
 
-    ; PORTB0,1 as outputs
-    ldi temp, 0x03
-    out DDRB, temp
+MAIN:
+    ; Initialize stack pointer
+    ldi r16, low(RAMEND)
+    out SPL, r16
+    ldi r16, high(RAMEND)
+    out SPH, r16
 
-MAIN_LOOP:
-    ; ---- Read inputs ----
-    in temp, PIND
-    lsr temp         ; shift to align PD2→bit0
-    lsr temp
-    andi temp, 0x07  ; mask bits 0..2 only
+    ; Set input pins PD2, PD3, PD4 as inputs (0)
+    in r16, DDRD
+    andi r16, ~(X_BIT | Y_BIT | Z_BIT) ; clear bits 2,3,4 to input
+    out DDRD, r16
 
-    mov xreg, temp
-    andi xreg, 0x01  ; isolate x
-    mov yreg, temp
-    lsr yreg
-    andi yreg, 0x01  ; isolate y
-    mov zreg, temp
-    lsr zreg
-    lsr zreg
-    andi zreg, 0x01  ; isolate z
+    ; Set output pins PD6, PD7 as outputs (1)
+    in r16, DDRD
+    ori r16, LED_A | LED_B
+    out DDRD, r16
 
-    ; ---- Compute LHS = (x OR y) XOR z ----
-    mov lhs, xreg
-    or  lhs, yreg      ; x OR y
-    eor lhs, zreg      ; XOR z
+LOOP:
+    ; Read PORTD inputs
+    in r17, PIND
 
-    ; ---- Compute RHS = x XOR (y OR z) ----
-    mov rhs, yreg
-    or  rhs, zreg      ; y OR z
-    eor rhs, xreg      ; XOR with x
+    ; Extract x, y, z bits and move to bit 0 in separate registers
+    ; x in r18, y in r19, z in r20 (0 or 1)
+    mov r18, r17
+    andi r18, X_BIT
+    lsr r18
+    lsr r18
+    lsr r18
 
-    ; ---- Write outputs to PORTB ----
-    ; PB0 = lhs, PB1 = rhs
-    clr temp
-    bst lhs,0
-    bld temp,0
-    bst rhs,0
-    bld temp,1
+    mov r19, r17
+    andi r19, Y_BIT
+    lsr r19
+    lsr r19
+    lsr r19
+    lsr r19
 
-    out PORTB, temp
+    mov r20, r17
+    andi r20, Z_BIT
+    lsr r20
+    lsr r20
+    lsr r20
+    lsr r20
 
-    rjmp MAIN_LOOP
+    ; Calculate x + y (OR)
+    mov r21, r18
+    or r21, r19  ; r21 = x OR y
+
+    ; Calculate y + z (OR)
+    mov r22, r19
+    or r22, r20  ; r22 = y OR z
+
+    ; Calculate A = (x + y) XOR z
+    mov r23, r21
+    eor r23, r20
+
+    ; Calculate B = x XOR (y + z)
+    mov r24, r18
+    eor r24, r22
+
+    ; Prepare PORTD output for LEDs
+    in r16, PORTD
+
+    ; Clear LED bits PD6, PD7
+    andi r16, ~(LED_A | LED_B)
+
+    ; Set LED_A bit if A=1
+    tst r23
+    breq SKIP_A
+    ori r16, LED_A
+SKIP_A:
+
+    ; Set LED_B bit if B=1
+    tst r24
+    breq SKIP_B
+    ori r16, LED_B
+SKIP_B:
+
+    out PORTD, r16
+
+    rjmp LOOP

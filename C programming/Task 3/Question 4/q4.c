25b9d15c2540841f72843e35d85afd600d433216
varshasajjan <varshinigonchigar@gmail.com>
2025-09-12 14:39:27 +0530

    Create q4.c

---
+++ C programming/Task 3/Question 4/q4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int IsPrime(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (IsPrime(n)) printf("%d is Prime\n", n);
+    else printf("%d is Not Prime\n", n);
+    return 0;
+}
